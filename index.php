30af408b975ae078fb11a58b95fda3e632c828cb
Teodora Stojanovic <teacine6@gmail.com>
2023-02-10 19:20:25 +0100

    sredjivanje pocetne stranice

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Knjizara</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+<body>
+
+<div class="content">
+        <header>
+            <a href="index.php"><img src="img/home.jpg" id="home"></a>
+        </header>
+        <section class="main-part">          
+            <h2 id="text1">Book shop</h2>
+            <a href="register.php" id="button-reglog">Register</a>
+            <a href="login.php" id="button-reglog">Log in</a>
+        </section>
+        </div>
+
+       
+</body>
+</html>
